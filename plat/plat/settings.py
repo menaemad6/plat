20091523ada2dbeaec35146f6a98ca7d302f73d5
Mena Emad <92929187+menaemad6@users.noreply.github.com>
2024-08-30 00:22:14 +0300

    Update settings.py

--- plat/plat/settings.py
+++ plat/plat/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@y#y7cg)ctrue@-qq^#ncke(jqhz78(#*z&3rs)rhlsa(@rp8#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
